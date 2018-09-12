05f80e3044a59670c502381eed9094ccb982fe8a
porebrique <porebrique@gmail.com>
2018-09-12 15:53:28 +0300

    fix

--- src/containers/game/index.js
+++ src/containers/game/index.js
@@ -2,10 +2,4 @@ import { createConnector } from 'src/helpers';
 import * as actions from 'src/actions'
 import Game from './Game';
 
-/*
-const actions = {
-  // TODO
-};
-*/
-
 export default createConnector({ actions })(Game);
